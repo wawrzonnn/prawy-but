f741bb724d532f47e86c81bcd7dce398943220ac
wawrzon <norbert.wawrzonek@gmail.com>
2025-10-04 12:56:14 +0200

    chore: bump app

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+	return (
+		<div className={styles.page}>
+			<h1>SIEMA JESTEM PRAWY BUT Z KALKUTY JESTEM SYMULACJĄ </h1>
+		</div>
+	)
+}
